c43e154d6ae97d3f4ae508017c6bb4e7b8cdb485
Jason Reilly <jdr0887@gmail.com>
2022-12-01 01:51:35 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# leverage the renci python base image
+FROM renciorg/renci-python-image:v0.0.1
+
+#Set the branch
+ARG BRANCH_NAME=main
+
+# install basic tools
+RUN apt-get update
+
+# make a directory for the repo
+RUN mkdir /repo
+
+# go to the directory where we are going to upload the repo
+WORKDIR /repo
+
+# get the latest code
+RUN git clone --branch $BRANCH_NAME --single-branch https://github.com/ranking-agent/aragorn.git
+
+# go to the repo dir
+WORKDIR /repo/aragorn
+
+# install requirements
+RUN pip install -r requirements.txt
+
+# expose the default port
+EXPOSE 4868
+
+RUN chmod 777 -R .
+
+USER nru
+
+# start the service entry point
+ENTRYPOINT ["bash", "main.sh"]
---
+++ src/server.py
@@ -0,0 +1,34 @@
+"""wrapper for aragorn and robokop."""
+
+from fastapi import Body, FastAPI, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
+from src.aragorn_app import ARAGORN_APP
+from src.robokop_app import ROBOKOP_APP
+from src.openapi_constructor import construct_open_api_schema
+
+#The app version is now going to be set in ../openapi-config.yaml
+#if you want to bump it, do it there.
+#Note that it's set once and applies to both robokop and aragorn (b/c they share much of their implementation)
+#APP_VERSION = '2.0.24'
+
+APP = FastAPI(title="ARAGORN/ROBOKOP")
+
+# Mount aragorn app at /aragorn
+APP.mount('/aragorn',  ARAGORN_APP, 'ARAGORN')
+# Mount robokop app at /robokop
+APP.mount('/robokop', ROBOKOP_APP, 'ROBOKOP')
+# Add all routes of each app for open api generation at /openapi.json
+# This will create an aggregate openapi spec.
+APP.include_router(ARAGORN_APP.router, prefix='/aragorn')
+APP.include_router(ROBOKOP_APP.router, prefix='/robokop')
+APP.openapi_schema = construct_open_api_schema(APP, description="ARAGORN/ROBOKOP workflow engine")
+
+# declare app access details
+APP.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
